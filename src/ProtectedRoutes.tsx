95d7b38ac18087f49c9fc1250a2cb24e4b03d371
HelloIAmBanana <oshridagan04@gmail.com>
2024-03-31 16:55:23 +0300

    Update ProtectedRoutes.tsx

--- src/ProtectedRoutes.tsx
+++ src/ProtectedRoutes.tsx
@@ -1,34 +1,14 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import AuthService from "./AuthService";
-import React from "react";
 import UserProvider from "./UserProvider";
-
-export const ProtectedRoutes = () => {
-  let isAuthenticated = AuthService.isUserAuthenticated();
-  return isAuthenticated ? (
-    <UserProvider>
-      <Outlet />
-    </UserProvider>
-  ) : (
-    <Navigate to="/signin" />
-  );
-};
-export const UnprotectedRoutes = () => {
-  let isAuthenticated = AuthService.isUserAuthenticated();
-  return isAuthenticated ? (
-    <UserProvider>
-        <Navigate to="/home" />
-    </UserProvider>
-  ) : (
-    <Outlet/>
-  );
-};
 
 export const AuthHandlerRoute = () => {
   const isAuthenticated = AuthService.isUserAuthenticated();
   const location = useLocation();
   const currentRoute = location.pathname;
-  const isPublicRoute = ["/signin", "/","/signup"].some(route => route === currentRoute);
+  const isPublicRoute = ["/signin", "/", "/signup"].some(
+    (route) => route === currentRoute
+  );
 
   if (isAuthenticated) {
     return isPublicRoute ? (
@@ -40,7 +20,7 @@ export const AuthHandlerRoute = () => {
         <Outlet />
       </UserProvider>
     );
-  }  
-  return <Outlet/>
-
-}
+  } else {
+    return isPublicRoute ? <Outlet /> : <Navigate to="/signin" />;
+  }
+};
