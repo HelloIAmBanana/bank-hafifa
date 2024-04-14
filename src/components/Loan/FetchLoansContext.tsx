35e8ca6f4b1eab322a0f8b43f7a0fb59219d2baf
HelloIAmBanana <oshridagan04@gmail.com>
2024-04-14 15:14:08 +0300

    Update FetchLoansContext.tsx

--- src/components/Loan/FetchLoansContext.tsx
+++ src/components/Loan/FetchLoansContext.tsx
@@ -1,29 +1,40 @@
-import React, {createContext, useContext, useState} from 'react';
-import { UserContext } from '../../UserProvider';
-import CRUDLocalStorage from '../../CRUDLocalStorage';
-import { Loan } from '../../models/loan';
+import React, { createContext, useContext, useMemo, useState } from "react";
+import { UserContext } from "../../UserProvider";
+import CRUDLocalStorage from "../../CRUDLocalStorage";
+import { Loan } from "../../models/loan";
+import AuthService from "../../AuthService";
 
 type FetchLoansContextType = {
-    fetchUserLoans: () => Promise<void>;
-    isLoansLoading: boolean;
-    loans: Loan[];
+  fetchUserLoans: () => Promise<void>;
+  isLoansLoading: boolean;
+  loans: Loan[];
 };
 
 const FetchLoansContext = createContext<FetchLoansContextType>({
-    fetchUserLoans: () => Promise.resolve(),
-    isLoansLoading: false,
-    loans: [],
+  fetchUserLoans: () => Promise.resolve(),
+  isLoansLoading: false,
+  loans: [],
 });
 
-export function FetchLoansContextProvider({ children }: React.PropsWithChildren)  {
-    const [currentUser] = useContext(UserContext);
-    const [isLoansLoading, setIsLoanLoading] = useState(true);
-    const [loans, setLoans] = useState<Loan[]>([]);
-    
+export function FetchLoansContextProvider({ children }: React.PropsWithChildren) {
+  const [currentUser] = useContext(UserContext);
+  const [isLoansLoading, setIsLoanLoading] = useState(true);
+  const [loans, setLoans] = useState<Loan[]>([]);
+  const isAdmin = useMemo(() => {
+    return AuthService.isUserAdmin(currentUser);
+  }, [currentUser]);
+
   const fetchUserLoans = async () => {
     setIsLoanLoading(true);
     try {
       const fetchedLoans = await CRUDLocalStorage.getAsyncData<Loan[]>("loans");
+
+      if (isAdmin) {
+        setLoans(fetchedLoans);
+        setIsLoanLoading(false);
+        return;
+      }
+
       const userLoans = fetchedLoans.filter((currentLoan) => currentLoan.accountID === currentUser!.id);
       setLoans(userLoans);
     } catch (error) {
@@ -33,7 +44,9 @@ export function FetchLoansContextProvider({ children }: React.PropsWithChildren)
   };
 
   return (
-    <FetchLoansContext.Provider value={{ fetchUserLoans: fetchUserLoans, isLoansLoading:isLoansLoading,loans:loans}}>
+    <FetchLoansContext.Provider
+      value={{ fetchUserLoans: fetchUserLoans, isLoansLoading: isLoansLoading, loans: loans }}
+    >
       {children}
     </FetchLoansContext.Provider>
   );
